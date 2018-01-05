a1ffc045fd509f71b5fe6fdd30431a537b5f35db
dcz6666 <dcz6666@126.com>
2018-01-05 17:47:13 +0800

    test

--- MatchModel.class.php
+++ MatchModel.class.php
@@ -4,8 +4,19 @@
    */
    class MatchModel{
    	// 获得所有的比赛列表
-   	publi function getList(){
-   		add 
-   	}
+   	public function getList(){
+   			$config=array(
+			 'host'=>'127.0.0.1',
+			 'port'=>'3306',
+			 'username'=>'root',
+			 'password'=>'123',
+			 'charset'=>'utf8',
+			 'dbname'=>'php34',
+			);
+   	   require 'MySQL.class.php';
+			$dao=MySQLDB::getInstance($config); //database access object 数据库操作对象（dao）
+			$sql="select t1.t_name as t1_name, m.t1_score, m.t2_score, t2.t_name as t2_name, m.m_time from `match` as m left join `team` as t1 ON m.t1_id = t1.t_id  left join `team` as t2 ON m.t2_id=t2.t_id;";
+			 return $dao->getAll($sql);
+		}
    }
  ?>
