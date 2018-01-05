516837a634c6856bfa8b8a1448c5d8786215c270
dcz6666 <dcz6666@126.com>
2018-01-05 17:57:29 +0800

    test

--- match_list_c.php
+++ match_list_c.php
@@ -1,7 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html;charset='UTF-8'">
 <?php 
-//调用相应的m获取比赛列表数据
-require './match_list_m.php';
+//实例化相应的模型类对象，调用某个方法实现固定功能
+require './MatchModel.class.php';
+ $m_match=new MatchModel();
+ $match_list=$m_match->getList();
 // 载入负责显示的html文件
 require './template/match_list_v.html';
  ?>
